03aefa8491c65f032585234a3ab806b6113389c1
mlouricas <32438745+mlouricas@users.noreply.github.com>
2017-11-13 12:19:04 -0600

    Add files via upload

---
+++ Plot3.R
@@ -0,0 +1,19 @@
+#Reading, naming and subsetting power consumption data
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+
+#Calling PNG Device, naming file and setting parameters
+png(filename = "Plot1.png", width = 480, height = 480)
+
+# calling the basic plot functions
+plot(subpower$Time,subpower$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_1))))
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+# annotating graph
+title(main="Energy sub-metering")
+
+dev.off()
